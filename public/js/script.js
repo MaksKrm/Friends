d36ef213d9a76fdf20c458ae049a2dc78d4ec7de
Vlad Ablashimov <vlad30101991@gmail.com>
2018-06-07 21:21:18 +0300

    correct ajax reports

--- public/js/script.js
+++ public/js/script.js
@@ -95,43 +95,4 @@ $(document).ready(function () {
         $('.form-group strong').text('');
     }
 
-    $(function () {
-        $('#subbut').live('click', function (e) {
-            e.preventDefault();
-            var url = $(this).attr('href');
-            getReport(url);
-        });
-
-        function getReport(url) {
-            $.ajax({
-                url: url,
-                data: $('#date_report').serialize(),
-            }).done(function (data) {
-                $('#types').html(data);
-            }).fail(function () {
-                alert('ошибка загрузки отчета');
-            });
-        }
-    });
-    $(function () {
-        $('.pagination a').live('click', function (e) {
-            e.preventDefault();
-
-            $('#load a').css('color', '#dfecf6');
-            var url = $(this).attr('href');
-            getArticles(url);
-            window.history.pushState("", "", url);
-        });
-
-        function getArticles(url) {
-            $.ajax({
-                url: url
-            }).done(function (data) {
-                $('.types').html(data);
-            }).fail(function () {
-                alert('ошибка загрузки');
-            });
-        }
-    });
-
 });
